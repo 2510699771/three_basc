4fe85a04d65c3c440139717c6eaf5f4b6d328e86
2510699771 <2510699771@qq.com>
2022-11-24 17:11:33 +0800

    gui ui控制组件

---
+++ src/main/main07.js
@@ -0,0 +1,138 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
+import * as dat from 'data.gui'
+
+// ui 控制组件
+
+// console.log(THREE,'THREE')
+
+// 1.创建场景
+const scene = new THREE.Scene()
+
+
+//  2.创建相机(透视相机)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+
+// 设置相机位置
+camera.position.set(0, 0, 10)
+
+// 加入场景
+scene.add(camera)
+
+// 创建几何体
+// 添加物体
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// 根据几何体和材质创建物体
+const cube = new THREE.Mesh(cubeGeometry, cubeMeshBasicMaterial);
+
+
+// 修改物体的位置
+// cube.position.set(5, 0, 0)
+// cube.position.x = 3
+
+// 缩放
+// cube.scale.set(3, 2, 1)
+// cube.scale.x = 3
+
+// 旋转
+// cube.rotation.set(Math.PI / 4, Math.PI / 2, Math.PI / 8, 'XZY')
+
+// 设置动画
+
+var animate1 = gsap.to(cube.position, {
+    x: 5,
+    duration: 5,
+    ease: 'power1.inOut',
+    repeat: 2,  // 循环次数  无限循环-1
+    yoyo: true,  // 往返运动
+    delay: 2,   // 延迟2s运动
+    onComplete: () => { console.log('动画完成'); },
+    onStart: () => { console.log('动画开始'); },
+})
+gsap.to(cube.rotation, { x: 2 * Math.PI, duration: 5 })
+
+// 暂停 恢复
+// window.addEventListener('dblclick', () => {
+//     console.log(animate1);
+//     if (animate1.isActive()) {
+//         // 暂停
+//         animate1.pause()
+//     } else {
+//         // 恢复
+//         animate1.resume()
+//     }
+// })
+
+
+
+// 将几何体添加到场景
+scene.add(cube)
+
+
+
+// 初始化渲染器
+const render = new THREE.WebGLRenderer()
+// 设置渲染尺寸大小
+render.setSize(window.innerWidth, window.innerHeight)
+
+// 将webgl渲染的canvas内容添加到body上
+document.body.appendChild(render.domElement)
+
+
+// 使用渲染器 通过相机 将场景渲染进来
+
+render.render(scene, camera)
+
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, render.domElement)
+
+// 设置阻尼 使控制器更有真实效果  必须在你的动画循环里调用.update()
+controls.enableDamping = true
+
+
+// 添加坐标轴辅助器
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+function rendeer(time) {
+    controls.update()  //更新阻尼
+    render.render(scene, camera);
+    //  渲染下一帧的时候就会调用rendeer函数
+    requestAnimationFrame(rendeer)
+}
+
+rendeer()
+
+
+// 全屏 or  退出全屏
+window.addEventListener('dblclick', () => {
+    //    双击控制屏幕进入全屏 退出全屏
+    const fullScreenElement = document.fullscreenElement
+    if (!fullScreenElement) {
+        render.domElement.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+})
+
+
+
+// 监听画面变化  更新渲染
+
+window.addEventListener('resize', () => {
+    console.log('画面更新了');
+
+    // 1.更新摄像头
+    camera.aspect = window.innerWidth / window.innerHeight
+    // 2.更新摄像机的投影矩阵
+    camera.updateProjectionMatrix()
+    // 3.更新渲染器
+    render.setSize(window.innerWidth, window.innerHeight)
+    // 4.设置渲染器像素比
+    render.setPixelRatio(window.devicePixelRatio)
+})
